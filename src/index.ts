40692521ceea7cc819368e37e9be9d8821ba1f67
shreeraj-shinde <shreerajshinde86@gmail.com>
2025-09-28 11:14:28 +0530

    dot evn added

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,10 @@ import express, { Request, Response } from "express";
 import { userRoutes } from "./routes/user.route";
 import { authRoutes } from "./routes/auth.route";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -10,19 +13,17 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 
 app.use("/users", userRoutes);
 app.use("/auth", authRoutes);
 
 // Routes
 app.get("/", (req: Request, res: Response) => {
-    
   res.send(`Hello TypeScript + Express + Yarn!`);
 });
 
-
 // Start Server
 app.listen(PORT, () => {
- 
   console.log(`🚀 Server running at http://localhost:${PORT}`);
 });
